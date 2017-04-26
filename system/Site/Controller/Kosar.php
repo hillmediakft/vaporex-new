9e2b44563519de17576e47c10af228d719bffb98
varnagy-zsolt <amigak240>
2017-04-26 19:14:28 +0200

    elem hozzadasa, elem torlese, minden elem torlese a kosarbol metodusok hozzaadva

--- system/Site/Controller/Kosar.php
+++ system/Site/Controller/Kosar.php
@@ -25,13 +25,79 @@ class Kosar extends SiteController {
         $data['keywords'] = $page_data['metakeywords'];
         $data['content'] = $page_data['body'];
        
-        $cart = new Cart();      
-        $data['items'] = $cart->getItems();
+        $data['items'] = $this->cart->getItems();
 
+        if (!empty($data['items'])) {
+            # code...
+        }
+
+
+//var_dump($data);
         $view = new View();
         $view->setHelper(array('url_helper', 'str_helper'));
         $view->add_link('js', $view->url_helper->autoVersion(SITE_JS . 'pages/kosar.js'));
         $view->render('kosar/tpl_kosar', $data);
+    }
+
+    /**
+     * Elem hozzáadása a kosárhoz
+     */
+    public function addItem()
+    {
+        if ($this->request->is_ajax()) {
+            $id = $this->request->get_post('product_id', 'integer');    
+            $qty = (int)$this->request->get_post('quantity');    
+            $qty = ($qty == 0) ? 1 : $qty;    
+            // hozzáadás a kosárhoz
+            $this->cart->add($id, $qty);
+            // kosár összes elemszáma
+            $items_number = $this->cart->getItemsNumber();
+
+            $this->response->json(array(
+                'status' => 'success',
+                'message' => $qty . ' elem hozzáadva a kosárhoz.',
+                'items_number' => $items_number,
+                'product_id' => $id
+                ));
+        }    
+    }
+
+    /**
+     * Elem kivétele a kosárból
+     */
+    public function removeItem()
+    {
+        if ($this->request->is_ajax()) {
+            $id = $this->request->get_post('product_id', 'integer');    
+            // elem eltávolítása a kosárból
+            $this->cart->remove($id);
+            // kosár összes elemszáma
+            $items_number = $this->cart->getItemsNumber();
+
+            $this->response->json(array(
+                'status' => 'success',
+                'message' => 'Elem eltávolítva a kosárból.',
+                'items_number' => $items_number,
+                'product_id' => $id
+                ));
+        }   
+    }
+
+    /**
+     * Kosár teljes tartalmának törlése
+     */
+    public function removeAllItem()
+    {
+        if ($this->request->is_ajax()) {
+            // kosár ürítése
+            $this->cart->clear();
+
+            $this->response->json(array(
+                'status' => 'success',
+                'message' => 'Kosár kiürítve.',
+                'items_number' => 0
+                ));
+        }   
     }
 
     /**
